619b354ca19c39a83230e8ebfb9162a4ca58924d
Songa7273 <k.gaju@alustudent.com>
2024-10-21 01:58:16 -0700

    prints the ASCII alphabet

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr((i - 97 + 65 * (i % 2)) % 26 + 97 - 32 * (i % 2)) for i in range(122, 96, -1))), end="")
+print("{}".format(
+    ''.join(chr((i - 65) % 26 + (65 if i % 2 else 97)) for i in range(90, 64, -1))
+), end="")
