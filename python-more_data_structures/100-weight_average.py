7130d44e521063d29b77448c0ba90fa42d182f51
Songa7273 <k.gaju@alustudent.com>
2024-10-23 01:01:09 -0700

    a function that returns the weighted average of all integers tuple

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -2,8 +2,8 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    
+
     numerator = sum(score * weight for score, weight in my_list)
     denominator = sum(weight for score, weight in my_list)
-    
+
     return float(numerator) / denominator
