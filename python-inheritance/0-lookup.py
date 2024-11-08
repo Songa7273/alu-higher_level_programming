9fb229cee37fde9949fdc5331d64c5103bf35c9b
Songa7273 <k.gaju@alustudent.com>
2024-11-08 01:07:51 -0800

    a function that returns the list of available attributes and methods of an object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+# 0-lookup.py
+"""Defines an object attribute lookup function."""
 
 
-class Rectangle:
-    """Represent a rectangle."""
-    pass
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
