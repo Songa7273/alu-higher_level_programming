a8faafcc14ccc5a575a7ab067d23feb15cf832b5
Songa7273 <k.gaju@alustudent.com>
2024-11-23 12:41:20 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://intranet.hbtn.io/status
+Python script that fetches https://alu-intranet.hbtn.io/status
 This script demonstrates the use of the requests package to make
 an HTTP GET request and display the response in a specific format.
-The script will:
-    - Make a GET request to the specified URL
-    - Display the response body type
-    - Display the response content
 """
 import requests
 
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
-    response = requests.get(url)
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    try:
+        url = "https://alu-intranet.hbtn.io/status"
+        response = requests.get(url)
+        print("Body response:")
+        print("\t- type: {}".format(type(response.text)))
+        print("\t- content: {}".format(response.text))
+    except requests.exceptions.RequestException:
+        # If there's an error with the ALU URL, try the alternate URL
+        url = "http://0.0.0.0:5050/status"
+        response = requests.get(url)
+        print("Body response:")
+        print("\t- type: {}".format(type(response.text)))
+        print("\t- content: {}".format(response.text))
