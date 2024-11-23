e62451804b66f18949622f2d0e61a064117979f8
Songa7273 <k.gaju@alustudent.com>
2024-11-23 12:03:43 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""
+Fetches a URL and displays the response body using the requests module.
+"""
+
 import requests
 
-url = "https://intranet.hbtn.io/status"
+def fetch_status(url):
+    """Fetches the status of a given URL and prints its type and content."""
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
 
-# Fetching the URL using requests
-response = requests.get(url)
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    fetch_status(url)
 
-# Displaying the response body
-print("Body response:")
-print("\t- type:", type(response.text))
-print("\t- content:", response.text)
