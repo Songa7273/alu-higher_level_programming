c591018539efc4f3a3e2f42d0174285fc76eb00e
Songa7273 <k.gaju@alustudent.com>
2024-11-23 12:38:18 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """
-Script that fetches a status URL using requests package
+Python script that fetches https://intranet.hbtn.io/status
+This script demonstrates the use of the requests package to make
+an HTTP GET request and display the response in a specific format.
+The script will:
+    - Make a GET request to the specified URL
+    - Display the response body type
+    - Display the response content
 """
 import requests
 
@@ -10,4 +16,4 @@ if __name__ == "__main__":
     response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text)
+    print("\t- content: {}".format(response.text))
