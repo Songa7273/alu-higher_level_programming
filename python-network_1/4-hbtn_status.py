bd902b964b304e7362c83faa75d37814bd4e4043
Songa7273 <k.gaju@alustudent.com>
2024-11-28 12:34:31 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
-"""
-Python script that fetches a status URL using requests package
-and displays the response body type and content.
+"""Fetches url
+using the request moduel
 """
 import requests
 
 
 if __name__ == "__main__":
-    try:
-        url = "https://intranet.hbtn.io/status"
-        response = requests.get(url)
-        print("Body response:")
-        print("\t- type: {}".format(type(response.text)))
-        print("\t- content: {}".format(response.text))
-    except requests.exceptions.ConnectionError:
-        try:
-            url = "https://alu-intranet.hbtn.io/status"
-            response = requests.get(url)
-            print("Body response:")
-            print("\t- type: {}".format(type(response.text)))
-            print("\t- content: {}".format(response.text))
-        except requests.exceptions.RequestException:
-            # If both URLs fail, use a default response for testing
-            print("Body response:")
-            print("\t- type: <class 'str'>")
-            print("\t- content: OK")
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
